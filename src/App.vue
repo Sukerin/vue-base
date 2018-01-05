2500abd964a533ef2521c43b4ea9810d449aba40
Doctor Rubby <563302070@qq.com>
2018-01-05 22:33:01 +0800

    can run

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-
+    <lucky-turntable></lucky-turntable>
     <div>
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
+  import luckyTurntable from './components/lucky turntable/Lucky_Turntable.vue'
   export default {
     name: 'app',
     data () {
@@ -19,12 +20,9 @@
       enterToProductList: function () {
         this.$router.push({path: '/list', query: {keyword: this.keyword}})
       }
-    }
+    },
+    components:{luckyTurntable},
+
   }
 </script>
 
-<style>
-  /*@import "./assets/public.css";*/
-  /*@import "./assets/searchList.css";*/
-
-</style>
